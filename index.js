28a3d390f94868634b241627da28725eaea44110
github username <github email address>
2020-05-21 16:42:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
-/* Your Code Here */
+function createEmployeeRecord(employee){
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees){
+    return employees.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(dateTime){
+    const [date, time] = dateTime.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(time),
+        date: date
+    })
+
+    return this
+}
+
+function createTimeOutEvent(dateTime){
+    const [date, time] = dateTime.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(time),
+        date: date
+    })
+
+    return this 
+}
+
+function hoursWorkedOnDate(formattedDate){
+    const timeIn = this.timeInEvents.find(workDate => workDate.date === formattedDate)
+    const timeOut = this.timeOutEvents.find(workDate => workDate.date === formattedDate)
+
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+function wagesEarnedOnDate(formattedDate){
+    const hoursWorked = hoursWorkedOnDate.call(this, formattedDate)     
+    const payOwed = hoursWorked * this.payPerHour
+    return payOwed
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -19,4 +69,13 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find(employee => employee.firstName === firstName)
+}
+
+function calculatePayroll(empRecordsArray){
+    const allWages = empRecordsArray.map(emp => allWagesFor.call(emp))
+    return allWages.reduce((wage, total) => wage + total)
 }
