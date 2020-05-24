58d9c6dbe69ab937f56072e7752ce10e21478ce6
LizLaffitte <liz.kossakowski@gmail.com>
2020-05-24 15:12:03 -0400

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,54 @@
  for you to use if you need it!
  */
 
+let createEmployeeRecord = function(employeeArray) {
+    return {
+        firstName: employeeArray[0], 
+        familyName: employeeArray[1], 
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(employeeData){
+    return employeeData.map(employee => createEmployeeRecord(employee))
+}
+
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    this.timeOutEvents.push(
+        {
+            type: 'TimeOut',
+            hour: parseInt(hour, 10),
+            date
+        }
+    )
+    return this
+}
+
+let hoursWorkedOnDate = function(dateStamp){
+    let timeIn = this.timeInEvents.find(function(event) {return event.date === dateStamp})
+    let timeOut = this.timeOutEvents.find(function(event) {return event.date === dateStamp})
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+let wagesEarnedOnDate = function(date){
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -20,3 +68,18 @@ let allWagesFor = function () {
 
     return payable
 }
+
+let findEmployeeByFirstName = function(employeeRecords, name){
+    return employeeRecords.find(element => element.firstName == name)
+}
+
+let calculatePayroll = function(employeeRecords){
+    return employeeRecords.reduce(function(runningTotal, record) {
+        return runningTotal + allWagesFor.call(record)
+    }, 0)
+}
+
+// let testEmployee = createEmployeeRecord(["Gray", "Worm", "Security", 1])
+// createTimeInEvent.call(testEmployee, "44-03-15 0900")
+// createTimeOutEvent.call(testEmployee, "44-03-15 1100")
+// console.log(hoursWorkedOnDate.call(testEmployee, "44-03-15"))
