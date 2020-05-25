0bace199e826e95f92d3918aea3c7d4f1fd011b5
github username <github email address>
2020-05-24 17:52:03 -0700

    Done.

--- index.js
+++ index.js
@@ -9,14 +9,93 @@
  for you to use if you need it!
  */
 
+ function createEmployeeRecord(record){
+     return {
+         firstName: record[0],
+         familyName: record[1],
+         title: record[2],
+         payPerHour: record[3],
+         timeInEvents: [],
+         timeOutEvents: []
+     }
+ }
+
+ function createEmployeeRecords(records){
+    return records.map(function(record){
+        return createEmployeeRecord(record);
+    })
+}
+
+let createTimeInEvent = function(timeIn){
+    let [date, time] = timeIn.split(' ');
+
+    this.timeInEvents.push(
+        {type: "TimeIn",
+         hour: parseInt(time, 10),
+         date}
+    )
+
+    return this;
+    
+}
+
+let createTimeOutEvent = function(timeOut){
+    let [date, time] = timeOut.split(' ')
+
+    this.timeOutEvents.push(
+        {type: "TimeOut",
+        hour: parseInt(time, 10),
+        date}
+    )
+
+    return this;
+}
+
+let hoursWorkedOnDate = function(desiredDate){
+    let clockedIn = 
+        this.timeInEvents.find(function(event){
+            return event.date === desiredDate;
+        })
+
+    let clockedOut = 
+        this.timeOutEvents.find(function(event){
+            return event.date === desiredDate;
+        })
+
+    return (clockedOut.hour - clockedIn.hour)/100
+}
+
+function wagesEarnedOnDate(desiredDate){
+    let employeeWage = hoursWorkedOnDate.call(this, desiredDate) * this.payPerHour
+    return employeeWage
+}
+
+
 let allWagesFor = function () {
+    let initialValue = 0
+
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    let allWagesPayable = eligibleDates.reduce(function (accumulator, date) {
+        return accumulator + wagesEarnedOnDate.call(this, date)
+    }.bind(this), initialValue) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
+    return allWagesPayable
+}
+
+
+
+let findEmployeeByFirstName = function(srcArray, firstName){
+    return srcArray.find(function(employeeRecord){
+        return employeeRecord.firstName === firstName;
+    })
+}
+
+let calculatePayroll = function(arrayofEmployeeRecords){
+    let initialValue = 0;
+    return arrayofEmployeeRecords.reduce(function(accumulator, record){
+        return accumulator + allWagesFor.call(record);
+    }, initialValue)
 }
